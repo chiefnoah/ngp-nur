88ce1c921a77b3b23dac4dbc0d81cec84197b3be
Noah Pederson <noah@packetlost.dev>
2026-06-15 13:00:26 -0500

    Fix meta for chicago95

--- pkgs/chicago95-theme/default.nix
+++ pkgs/chicago95-theme/default.nix
@@ -1,9 +1,24 @@
-{ symlinkJoin, chicago95 }:
+{ symlinkJoin, chicago95, lib }:
 
 symlinkJoin {
   name = "chicago95-theme-${chicago95.version}";
   paths = [ chicago95 ];
-  meta = chicago95.meta // {
+
+  meta = {
     description = "Chicago95 retro desktop theme packaged for NGP systems";
+    homepage = "https://github.com/grassmunk/Chicago95";
+    license = with lib.licenses; [
+      gpl3Plus
+      mit
+    ];
+    maintainers = [
+      {
+        email = "noah@packetlost.dev";
+        github = "chiefnoah";
+        githubId = 3588683;
+        name = "Noah Pederson";
+      }
+    ];
+    platforms = lib.platforms.linux;
   };
 }
