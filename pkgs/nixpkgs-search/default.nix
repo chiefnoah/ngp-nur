61d136033f51e43b43f10d3d53ed98ee5dc2f94e
Noah Pederson <noah@packetlost.dev>
2026-09-06 12:29:18 -0500

    Change forge URI for nixpkgs-search

--- pkgs/nixpkgs-search/default.nix
+++ pkgs/nixpkgs-search/default.nix
@@ -11,7 +11,7 @@ buildGoModule {
   version = "0.1.0";
 
   src = fetchgit {
-    url = "https://git.sr.ht/~chiefnoah/nixpkgs-search";
+    url = "https://tangled.org/ngp.computer/nixpkgs-search";
     rev = "767590ba41174e97f180f8a6d0fbdcf6b4286548";
     hash = "sha256-1ZoFXiLah0wwgPI8K2Ao91XXN+ZhQQ/y+A/5pWu5wow=";
   };
